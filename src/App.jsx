81cd64cfc476ca0d4807e6e3f50c74e429ee1852
rithasahmed12 <ahmedrithas48@gmail.com>
2024-03-12 07:51:02 +0530

    navbar made

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Main = () => {
+
+    const [movies, setMovies] = useState
+
+  return (
+    <div>
+      Main
+    </div>
+  )
+}
+
+export default Main
+Main
